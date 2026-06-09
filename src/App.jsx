83fb6b9a03e14c4fb95c291bad66533a49ca4ec7
Dhen Bagus <hadibagus225@gmail.com>
2026-06-09 22:16:37 +0700

    Change router to HashRouter and update Vite base path for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Flight from "./pages/Flight";
 import Train from "./pages/Train";
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Design-FrontEnd-Web-Travel-Boking-PAKDE-TOUR-/'
+  base: './'   // ← GANTI JADI INI (titik dan garis miring)
 })
